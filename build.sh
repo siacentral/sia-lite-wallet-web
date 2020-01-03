d69f850a7b505d02958485c8365df01b17bffb2b
Nate Maninger <me@n8m.us>
2020-01-03 20:26:04 +0900

    update gitignore to remove build file

--- build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t siacentral/web/ledger .
-docker tag siacentral/web/ledger:latest 921261815063.dkr.ecr.us-east-2.amazonaws.com/siacentral/web/ledger:latest
-docker push 921261815063.dkr.ecr.us-east-2.amazonaws.com/siacentral/web/ledger:latest
