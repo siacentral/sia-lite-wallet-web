02c63582006d76c7f9934d9f3cceb3b198a20195
Nate Maninger <me@n8m.us>
2020-01-04 21:34:20 +0900

    add contract and contract revisions

--- wasm/go.mod
+++ wasm/go.mod
@@ -3,8 +3,7 @@ module github.com/siacentral/sia-lite/wasm
 go 1.13
 
 require (
-	github.com/coreos/bbolt v1.3.2 // indirect
-	github.com/siacentral/apiclient v0.0.0-20200103105606-68ebf28da3a9
+	github.com/siacentral/apiclient v0.0.0-20200104031733-bc723ac6a6b9
 	gitlab.com/NebulousLabs/Sia v1.4.2-0.20191220232351-91e83488aaa4
 	gitlab.com/NebulousLabs/entropy-mnemonics v0.0.0-20181018051301-7532f67e3500
 	gitlab.com/NebulousLabs/fastrand v0.0.0-20181126182046-603482d69e40
--- wasm/main.go
+++ wasm/main.go
@@ -17,29 +17,58 @@ import (
 )
 
 type (
-	processedTransaction struct {
-		TransactionID  string            `json:"transaction_id"`
-		Direction      string            `json:"direction"`
-		BlockHeight    uint64            `json:"block_height"`
-		Confirmations  uint64            `json:"confirmations"`
-		Fees           siatypes.Currency `json:"fees"`
-		Value          siatypes.Currency `json:"value"`
-		Timestamp      time.Time         `json:"timestamp"`
-		Tags           []string          `json:"tags"`
-		SiacoinInputs  []processedInput  `json:"siacoin_inputs"`
-		SiacoinOutputs []processedOutput `json:"siacoin_outputs"`
+	//StorageContract a storage contract on the blockchain
+	processedContract struct {
+		ID                     string            `json:"id"`
+		BlockID                string            `json:"block_id"`
+		TransactionID          string            `json:"transaction_id"`
+		MerkleRoot             string            `json:"merkle_root"`
+		UnlockHash             string            `json:"unlock_hash"`
+		Status                 string            `json:"status"`
+		RevisionNumber         uint64            `json:"revision_number"`
+		NegotiationHeight      uint64            `json:"negotiation_height"`
+		ExpirationHeight       uint64            `json:"expiration_height"`
+		ProofDeadline          uint64            `json:"proof_deadline"`
+		ProofHeight            uint64            `json:"proof_height"`
+		Payout                 siatypes.Currency `json:"payout"`
+		FileSize               siatypes.Currency `json:"file_size"`
+		ValidProofOutputs      []processedOutput `json:"valid_proof_outputs"`
+		MissedProofOutputs     []processedOutput `json:"missed_proof_outputs"`
+		NegotiationTimestamp   time.Time         `json:"negotiation_timestamp"`
+		ExpirationTimestamp    time.Time         `json:"expiration_timestamp"`
+		ProofDeadlineTimestamp time.Time         `json:"proof_deadline_timestamp"`
+		ProofTimestamp         time.Time         `json:"proof_timestamp"`
+		ProofConfirmed         bool              `json:"proof_confirmed"`
+		Unused                 bool              `json:"unused"`
 	}
-	
+
+	processedTransaction struct {
+		TransactionID     string                  `json:"transaction_id"`
+		Direction         string                  `json:"direction"`
+		BlockHeight       uint64                  `json:"block_height"`
+		Confirmations     uint64                  `json:"confirmations"`
+		Fees              siatypes.Currency       `json:"fees"`
+		Value             siatypes.Currency       `json:"value"`
+		Timestamp         time.Time               `json:"timestamp"`
+		Tags              []string                `json:"tags"`
+		SiacoinInputs     []processedInput        `json:"siacoin_inputs"`
+		SiacoinOutputs    []processedOutput       `json:"siacoin_outputs"`
+		Contracts         []processedContract     `json:"contracts"`
+		ContractRevisions []processedContract     `json:"contract_revisions"`
+		StorageProofs     []apitypes.StorageProof `json:"storage_proofs"`
+		HostAnnouncements []apitypes.Announcement `json:"host_announcements"`
+	}
+
 	processedOutput struct {
 		apitypes.SiacoinOutput
 		Owned bool `json:"owned"`
 	}
-	
+
 	processedInput struct {
 		apitypes.SiacoinInput
 		Owned bool `json:"owned"`
 	}
-	
+
 	transactionResp struct {
 		Transactions     []processedTransaction   `json:"transactions"`
 		UnspentOutputs   []apitypes.SiacoinOutput `json:"unspent_outputs"`
@@ -464,17 +493,101 @@ func getTransactions(this js.Value, args []js.Value) interface{} {
 		}
 
 		for _, txn := range transactions {
-			processed := processedTransaction{
-				TransactionID: txn.TransactionID,
-				BlockHeight:   txn.BlockHeight,
-				Confirmations: txn.Confirmations,
-				Timestamp:     txn.Timestamp,
-				Tags:          txn.Tags,
-				Fees:          txn.Fees,
-			}
-
 			var ownedInput, ownedOutput siatypes.Currency
 			var ownedInputsCount, ownedOutputsCount int
+
+			processed := processedTransaction{
+				TransactionID:     txn.TransactionID,
+				BlockHeight:       txn.BlockHeight,
+				Confirmations:     txn.Confirmations,
+				Timestamp:         txn.Timestamp,
+				Tags:              txn.Tags,
+				Fees:              txn.Fees,
+				StorageProofs:     txn.StorageProofs,
+				HostAnnouncements: txn.HostAnnouncements,
+				Contracts: make([]processedContract, len(txn.Contracts)),
+				ContractRevisions: make([]processedContract, len(txn.ContractRevisions)),
+			}
+
+			for i, contract := range txn.Contracts {
+				procContract := processedContract{
+					ID:                     contract.ID,
+					BlockID:                contract.BlockID,
+					TransactionID:          contract.TransactionID,
+					MerkleRoot:             contract.MerkleRoot,
+					UnlockHash:             contract.UnlockHash,
+					Status:                 contract.Status,
+					RevisionNumber:         contract.RevisionNumber,
+					NegotiationHeight:      contract.NegotiationHeight,
+					ExpirationHeight:       contract.ExpirationHeight,
+					ProofDeadline:          contract.ProofDeadline,
+					ProofHeight:            contract.ProofHeight,
+					Payout:                 contract.Payout,
+					FileSize:               contract.FileSize,
+					ValidProofOutputs:      make([]processedOutput, len(contract.ValidProofOutputs)),
+					MissedProofOutputs:     make([]processedOutput, len(contract.MissedProofOutputs)),
+					NegotiationTimestamp:   contract.NegotiationTimestamp,
+					ExpirationTimestamp:    contract.ExpirationTimestamp,
+					ProofDeadlineTimestamp: contract.ProofDeadlineTimestamp,
+					ProofTimestamp:         contract.ProofTimestamp,
+					ProofConfirmed:         contract.ProofConfirmed,
+					Unused:                 contract.Unused,
+				}
+
+				for j, output := range contract.ValidProofOutputs {
+					_, exists := ownedAddresses[output.UnlockHash]
+					procContract.ValidProofOutputs[j].SiacoinOutput = output
+					procContract.ValidProofOutputs[j].Owned = exists
+				}
+
+				for j, output := range contract.MissedProofOutputs {
+					_, exists := ownedAddresses[output.UnlockHash]
+					procContract.MissedProofOutputs[j].SiacoinOutput = output
+					procContract.MissedProofOutputs[j].Owned = exists
+				}
+
+				processed.Contracts[i] = procContract
+			}
+
+			for i, contract := range txn.ContractRevisions {
+				procContract := processedContract{
+					ID:                     contract.ID,
+					BlockID:                contract.BlockID,
+					TransactionID:          contract.TransactionID,
+					MerkleRoot:             contract.MerkleRoot,
+					UnlockHash:             contract.UnlockHash,
+					Status:                 contract.Status,
+					RevisionNumber:         contract.RevisionNumber,
+					NegotiationHeight:      contract.NegotiationHeight,
+					ExpirationHeight:       contract.ExpirationHeight,
+					ProofDeadline:          contract.ProofDeadline,
+					ProofHeight:            contract.ProofHeight,
+					Payout:                 contract.Payout,
+					FileSize:               contract.FileSize,
+					ValidProofOutputs:      make([]processedOutput, len(contract.ValidProofOutputs)),
+					MissedProofOutputs:     make([]processedOutput, len(contract.MissedProofOutputs)),
+					NegotiationTimestamp:   contract.NegotiationTimestamp,
+					ExpirationTimestamp:    contract.ExpirationTimestamp,
+					ProofDeadlineTimestamp: contract.ProofDeadlineTimestamp,
+					ProofTimestamp:         contract.ProofTimestamp,
+					ProofConfirmed:         contract.ProofConfirmed,
+					Unused:                 contract.Unused,
+				}
+
+				for j, output := range contract.ValidProofOutputs {
+					_, exists := ownedAddresses[output.UnlockHash]
+					procContract.ValidProofOutputs[j].SiacoinOutput = output
+					procContract.ValidProofOutputs[j].Owned = exists
+				}
+
+				for j, output := range contract.MissedProofOutputs {
+					_, exists := ownedAddresses[output.UnlockHash]
+					procContract.MissedProofOutputs[j].SiacoinOutput = output
+					procContract.MissedProofOutputs[j].Owned = exists
+				}
+
+				processed.ContractRevisions[i] = procContract
+			}
 
 			for _, txnSiacoinInput := range txn.SiacoinInputs {
 				procSiacoinInput := processedInput{
