72f4f997056e544568b14cad6ed62ed38672b2dc
ps2xu <ps2xu@163.com>
2020-01-11 21:53:03 +0800

    Update cn.js
    2020-1-11

--- src/translation/languages/cn.js
+++ src/translation/languages/cn.js
@@ -8,48 +8,48 @@ export default {
 		getStarted: '继续',
 		addWallet: '增加钱包',
 		rescanWallet: '重新扫描钱包',
-		addAddresses: 'Add Addresses',
+		addAddresses: '增加地址',
 		exportSeed: '输入种子',
 		deleteWallet: '删除钱包',
 		unlockWallet: '解锁钱包',
 		walletPassword: '钱包密码',
-		transactionFee: 'Transaction Fee',
+		transactionFee: '交易费',
 		total: '共计',
-		minerFee: 'Miner Fee',
+		minerFee: '矿工费',
 		address: '地址',
-		amount: 'Amount',
-		summary: 'Summary',
-		inputs: 'Inputs',
-		outputs: 'Outputs',
+		amount: '金额',
+		summary: '概况',
+		inputs: '输入',
+		outputs: '输出',
 		add: '增加',
-		connect: 'Connect',
+		connect: '连接',
 		send: '发送',
 		receive: '接收',
 		verify: '校验',
 		unlock: '解锁',
 		done: '确认',
 		next: '继续',
-		import: '进口',
+		import: '输入',
 		generate: '生成',
 		recover: '恢复',
 		delete: '删除',
 		cancel: '取消',
-		sign: 'Sign',
-		requiredSignatures: 'Required Signatures',
-		status: 'Status',
-		wallet: 'Wallet',
-		unconfirmed: 'Unconfirmed',
+		sign: '签名',
+		requiredSignatures: '必需的签名',
+		status: '状态',
+		wallet: '钱包',
+		unconfirmed: '未确认',
 		walletStatus: {
-			scanning: 'Scanning...',
-			queued: 'Scan Queued...'
+			scanning: '扫描中...',
+			queued: '列队扫描...'
 		},
 		deleteWalletModal: {
 			pDeleteConfirm: '您确定要删除名为“{{0}}”的钱包吗？ 这将从您的设备中删除与此钱包相关的所有数据。 请确保您已备份恢复种子。',
 			deleteHeader: '删除“{{0}}”？'
 		},
 		menu: {
-			wallets: 'Wallets',
-			settings: 'Settings'
+			wallets: '钱包',
+			settings: '设置'
 		},
 		setup: {
 			welcomeHeader: '欢迎来到Sia Central轻钱包',
@@ -78,72 +78,72 @@ export default {
 			pExplainRounds: '减少次数或地址数可以提高性能，但会增加找不到地址或余额的机会。使用当前设置，在完成之前至少使用{{0}}个连续地址索引。'
 		},
 		unlockWalletModal: {
-			pWalletsLocked: '您的钱包目前已加密并锁定。输入密码解锁您的钱包并查看您的资金。'
+			pWalletsLocked: '您的钱包目前已加密并锁定，输入密码解锁您的钱包并查看您的资金。'
 		},
 		createWalletModal: {
 			newWallet: '新建钱包',
 			recoverWallet: '恢复钱包',
 			ledgerWallet: 'Ledger硬件钱包',
 			watchWallet: '只读钱包',
-			pNewWalletExplain: '在浏览器中生成一个新的钱包种子。 可以发送和接收交易。',
-			pRecoverWalletExplain: '用29字的种子中恢复现有的钱包。 可以发送和接收交易。',
-			pCreateLedgerExplain: 'Creates a new hardware backed wallet. All transactions must be signed by the Ledger device.',
+			pNewWalletExplain: '在浏览器中生成一个新的钱包种子， 可以发送和接收交易。',
+			pRecoverWalletExplain: '用29字的种子中恢复现有的钱包， 可以发送和接收交易。',
+			pCreateLedgerExplain: '创建一个新的硬件支持的钱包，所有交易必须由Ledger设备签名。',
 			pLedgerUnsupportedExplain: 'Ledger硬件钱包仅支持在Chrome浏览器中使用。 在Chrome中启用“Experimental Web Platform Features”并连接到Ledger设备。',
 			pWatchWalletExplain: '创建一个新的只读钱包。 必须手动添加地址，并且无法发送交易。',
-			pReviewLedger: 'Your Ledger wallet has been imported. Balance and transactions can now be viewed without the Ledger device. To send transactions you will need to plugin and unlock the device.',
+			pReviewLedger: '您的Ledger钱包已导入。现在无需分类帐设备即可查看余额和交易。要发送交易，您需要插入并解锁设备。',
 			pReviewWatch: '已创建一个新的只读钱包。 该钱包无法发送交易。 将显示所有导入地址的余额和交易。',
 			pReviewNew: '恭喜您！成功创建了一个新的钱包。请将您的恢复种子备份到安全的位置。没有种子，您的资金将无法恢复。',
-			pReviewRecover: 'Your wallet has been successfully recovered. The blockchain is now being scanned for balance and transactions. Please backup your recovery seed to a safe location, without your seed your funds cannot be recovered.',
+			pReviewRecover: '恭喜您！钱包已成功恢复。现在正在扫描余额和交易。请备份您的恢复种子到一个安全的位置，没有种子，您的资金将无法恢复。',
 			lblWalletName: '钱包名称',
 			lblRecoverySeed: '恢复种子',
-			lblSeedType: 'Seed Type',
-			siaSeed: 'Sia Seed',
-			walrusSeed: 'Walrus Seed'
+			lblSeedType: '种子类型',
+			siaSeed: 'Sia种子',
+			walrusSeed: 'Walrus种子'
 
 		},
 		importAddresses: {
 			importedHeader: '输入地址',
 			balance: '当前余额',
-			addressPlaceholder: 'Sia address to watch...',
-			publicKey: 'Import Public Key',
+			addressPlaceholder: '查看的Sia地址...',
+			publicKey: '导入公钥',
 			addAddress: '增加地址'
 		},
 		ledger: {
-			connected: 'Connected',
-			disconnected: 'Disconnected',
-			instructions: 'Current Balance'
+			connected: '已连接',
+			disconnected: '已断开',
+			instructions: '当前余额'
 		},
 		sendSiacoinsModal: {
-			spendableBalance: 'Spendable Balance',
-			recipientAddress: 'Recipient Address',
-			spent: 'Spent',
-			remainingBalance: 'Remaining Balance',
-			txtRecipientPlaceholder: 'Send Siacoin to...',
-			errorGreaterThan0: 'Must send more than 0 SC',
-			errorNotEnough: 'Send amount more than balance',
-			errorHighFee: 'Amount sent is less than transaction fees',
-			errorBadRecipient: 'Invalid recipient address',
-			statusSigning: 'Signing transaction...',
-			statusBroadcasting: 'Broadcasting transaction...'
+			spendableBalance: '可用余额',
+			recipientAddress: '接收地址',
+			spent: '花费',
+			remainingBalance: '余额',
+			txtRecipientPlaceholder: '发送Siacoin到...',
+			errorGreaterThan0: '至少发送0个以上SC',
+			errorNotEnough: '发送金额超过余额',
+			errorHighFee: '发送的金额少于交易费',
+			errorBadRecipient: '无效的接收地址',
+			statusSigning: '正在签署交易...',
+			statusBroadcasting: '正在广播交易...'
 		},
 		transactionTypes: {
-			contractRevision: 'Contract Revision',
-			contractFormation: 'Contract Formation',
-			storageProof: 'Storage Proof',
-			hostAnnouncement: 'Host Announcement',
-			contractCompleted: 'Contract Completion',
-			blockReward: 'Block Reward',
+			contractRevision: '合同修订',
+			contractFormation: '合同形成',
+			storageProof: '储存证明',
+			hostAnnouncement: '主机宣布',
+			contractCompleted: '合同完成',
+			blockReward: '区块奖励',
 			siacoinTransaction: 'Siacoin交易',
-			siafundTransaction: 'Siafund Transaction',
+			siafundTransaction: 'Siafund交易',
 			siafundClaim: 'Siafund Claim',
-			defrag: 'Defrag'
+			defrag: '碎片整理'
 		},
 		outputTags: {
-			received: 'Received',
-			apiFee: 'API Fee',
-			recipient: 'Recipient',
-			sent: 'Sent',
-			sender: 'Sender'
+			received: '已收到',
+			apiFee: 'API费',
+			recipient: '接收者',
+			sent: '已发送',
+			sender: '发送者'
 		},
 		currency: {
 			fiat: '法定货币',
@@ -165,18 +165,18 @@ export default {
 			ltc: '莱特币'
 		},
 		alerts: {
-			welcomeBack: 'Wallets decrypted. Welcome back!',
-			unlockError: 'Unable to decrypt wallets. Incorrect password',
-			unlockSuccess: 'Wallets decrypted. Welcome back!',
-			rescanWallet: 'Wallet has been queued for rescan',
-			transactionBroadcast: 'Transaction broadcast successfully',
-			addressesAdded: 'Addresses added. Wallet scan queued.',
-			walletsLocked: 'Wallets automatically locked due to inactivity'
+			welcomeBack: '钱包已解锁，欢迎回来！',
+			unlockError: '无法解锁钱包，密码错误！',
+			unlockSuccess: '钱包已解密。欢迎回来！',
+			rescanWallet: '钱包已等待重新扫描。',
+			transactionBroadcast: '广播交易成功。',
+			addressesAdded: '已增加地址，钱包扫描已排队。',
+			walletsLocked: '因不活动钱包已自动锁定。'
 		},
 		language: {
-			automatic: 'Automatic',
+			automatic: '自动',
 			en: 'English',
-			cn: 'Chinese'
+			cn: '中文'
 		}
 	}
 };
