7ae4316ccdacf93a3661a13fea032abdfbadc1b9
Nate Maninger <me@n8m.us>
2020-03-18 18:43:46 +0900

    update walrus wallet balance

--- src/api/walrus.js
+++ src/api/walrus.js
@@ -1,4 +1,5 @@
 import { sendJSONRequest } from './common';
+import BigNumber from 'bignumber.js';
 
 function singleJoiningSlash(a, b) {
 	const aslash = a[a.length - 1] === '/',
@@ -21,7 +22,6 @@ export default class {
 	}
 
 	async getAddresses() {
-		console.log(this._baseURL, '/addresses', singleJoiningSlash(this._baseURL, '/addresses'));
 		const addresses = await sendJSONRequest(singleJoiningSlash(this._baseURL, '/addresses'), {
 			method: 'GET'
 		});
@@ -45,35 +45,52 @@ export default class {
 		if (unlockConds.statusCode < 200 || unlockConds.statusCode >= 300)
 			throw new Error(unlockConds.body);
 
-		if (!Array.isArray(unlockConds.body))
+		if (typeof unlockConds.body !== 'object')
 			return [];
 
-		return addresses.reduce((uc, a) => {
+		const processed = addresses.reduce((uc, a) => {
+			const conditions = unlockConds.body[a];
+
 			// only supports unlock conditons with 1 signature required for now
-			if (unlockConds[a] && unlockConds[a].unlockConditions.signaturesRequired === 1) {
+			if (conditions && conditions.unlockConditions.signaturesRequired === 1) {
 				uc.push({
 					address: a,
 					unlock_conditions: {
-						publickeys: unlockConds[a].unlockConditions.publicKeys,
-						signaturesrequired: unlockConds[a].unlockConditions.signaturesRequired
+						publickeys: conditions.unlockConditions.publicKeys,
+						signaturesrequired: conditions.unlockConditions.signaturesRequired
 					},
-					index: unlockConds[a].keyIndex
+					index: conditions.keyIndex
 				});
 			}
 
 			return uc;
 		}, []);
+
+		processed.sort((a, b) =>
+			a.index > b.index ? -1 : a.index < b.index ? 1 : 0);
+
+		return processed;
 	}
 
 	async getUnspentOutputs() {
-		const resp = await sendJSONRequest(singleJoiningSlash(this._baseURL, '/utxos'), {
+		const resp = await sendJSONRequest(singleJoiningSlash(this._baseURL, '/utxos?limbo=true'), {
 			method: 'GET'
 		});
 
 		if (resp.statusCode < 200 || resp.statusCode >= 300)
 			throw new Error(resp.body);
 
-		return resp.body;
+		if (!Array.isArray(resp.body))
+			return [];
+
+		return resp.body.map(u => ({
+			output_id: u.ID,
+			unlock_hash: u.unlockHash,
+			source: 'siacoin_transaction',
+			maturity_height: 0,
+			block_height: 0,
+			value: u.value
+		}));
 	}
 
 	async broadcastTransaction(txn) {
@@ -103,9 +120,11 @@ export default class {
 		return resp.body;
 	}
 
-	async getTransactions() {
-		const transactions = await sendJSONRequest(singleJoiningSlash(this._baseURL, '/batchquery/transactions'), {
-			method: 'POST'
+	async getTransactions(max) {
+		max = max || 500;
+
+		const transactions = await sendJSONRequest(singleJoiningSlash(this._baseURL, `/transactions?max=${max}`), {
+			method: 'GET'
 		});
 
 		if (transactions.statusCode < 200 || transactions.statusCode >= 300)
@@ -113,6 +132,68 @@ export default class {
 
 		if (!Array.isArray(transactions.body))
 			return [];
+
+		const txDetails = await sendJSONRequest(singleJoiningSlash(this._baseURL, '/batchquery/transactions'), {
+			method: 'POST',
+			body: transactions.body
+		});
+
+		if (txDetails.statusCode < 200 || txDetails.statusCode >= 300)
+			throw new Error(txDetails.body);
+
+		if (typeof txDetails.body !== 'object')
+			return [];
+
+		return transactions.body.reduce((transactions, id) => {
+			const txn = txDetails.body[id];
+
+			if (!txn)
+				return transactions;
+
+			const processed = {
+					transaction_id: id,
+					block_height: parseInt(txn.blockHeight, 10),
+					fees: txn.minerFees,
+					siafund_inputs: [],
+					siafund_outputs: [],
+					timestamp: new Date(txn.timestamp)
+				},
+				inflow = new BigNumber(txn.inflow),
+				outflow = new BigNumber(txn.outflow);
+
+			if (inflow.gt(outflow)) {
+				processed.siacoin_value = {
+					direction: 'received',
+					value: inflow.toString(10)
+				};
+			} else {
+				processed.siacoin_value = {
+					direction: 'sent',
+					value: outflow.minus(inflow).toString(10)
+				};
+			}
+
+			if (Array.isArray(txn.siacoinOutputs)) {
+				processed.siacoin_outputs = txn.siacoinOutputs.map(o => ({
+					value: o.value,
+					unlock_hash: o.unlockHash
+				}));
+			}
+
+			if (Array.isArray(txn.siacoinInputs)) {
+				processed.siacoin_inputs = txn.siacoinInputs.map(i => ({
+					parent_id: i.parentID,
+					unlock_conditions: {
+						publickeys: i.unlockConditions.publicKeys,
+						signaturesrequired: i.signaturesRequired
+					}
+				}));
+			}
+
+			transactions.push(processed);
+
+			return transactions;
+		}, []);
 	}
 
 	async addUnlockConditions(unlockConditions, keyIndex) {
--- src/sync/walrus.js
+++ src/sync/walrus.js
@@ -1,6 +1,6 @@
 import WalrusClient from '@/api/walrus';
 import { saveAddresses } from '@/store/db';
-// import Store from '@/store';
+import Store from '@/store';
 
 async function scan(wallet) {
 	const walrus = new WalrusClient(wallet.server_url),
@@ -16,6 +16,17 @@ export default {
 	quickScan: scan,
 	fullScan: scan,
 	scanTransactions: async function(wallet) {
+		const walrus = new WalrusClient(wallet.server_url),
+			values = await Promise.all([
+				walrus.getTransactions(500),
+				walrus.getUnspentOutputs(),
+				walrus.getBalance(true)
+			]);
 
+		wallet.transactions = values[0] || [];
+		wallet.unspent_siacoin_outputs = values[1] || [];
+		wallet.confirmed_siacoin_balance = values[2] || '0';
+
+		await Store.dispatch('saveWallet', wallet);
 	}
 };
