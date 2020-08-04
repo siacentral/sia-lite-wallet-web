ab8f0c6d582f458395566f16e3b2514b6214ee4a
Nate Maninger <me@n8m.us>
2020-08-05 07:35:00 +0900

    SC to cryptocurrencies

--- src/translation/languages/en.js
+++ src/translation/languages/en.js
@@ -80,7 +80,7 @@ export default {
 		},
 		setup: {
 			header: 'Get Started',
-			caution1: 'Use caution when sending or receiving Siacoins to prevent coin loss. Siacoin transactions are permanent and non-reversable. Sia Central is not responsible for any damages incurred while using this wallet.',
+			caution1: 'Use caution when sending or receiving cryptocurrencies to prevent coin loss. Cryptocurrency transactions are permanent and non-reversable. Sia Central is not responsible for any damages incurred while using this wallet.',
 			caution2: 'Report any bugs or submit feedback on GitHub.',
 			password1: 'Set a secure password to encrypt your wallets with. This password will be required to unlock and use your wallets. All wallet seeds are encrypted, stored locally and never leave the device.'
 		},
